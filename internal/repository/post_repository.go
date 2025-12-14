3f15c868222dd2a59c3ff763a0da58de272b6916
crist <cr1st4ever@outlook.com>
2025-12-14 16:38:00 +0800

    feat(post_repository): add some basic function of post repository

---
+++ internal/repository/post_repository.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"crist-blog/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type PostRepository struct {
+	DB *gorm.DB
+}
+
+func NewPostRepository(db *gorm.DB) *PostRepository {
+	return &PostRepository{DB: db}
+}
+
+func (r *PostRepository) CreatePost(post *model.Post) error {
+	return r.DB.Create(post).Error
+}
+
+func (r *PostRepository) GetByID(id string) (*model.Post, error) {
+	var post model.Post
+	err := r.DB.Where("id = ?", id).First(&post).Error
+	return &post, err
+}
+
+func (r *PostRepository) Update(post *model.Post) error {
+	return r.DB.Save(post).Error
+}
+
+func (r *PostRepository) Delete(id string) error {
+	return r.DB.Where("id = ?", id).Delete(&model.Post{}).Error
+}
+
+func (r *PostRepository) List() ([]*model.Post, error) {
+	var posts []*model.Post
+	return posts, r.DB.Find(&posts).Error
+}
